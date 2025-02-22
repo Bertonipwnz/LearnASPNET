9f8b23a5690da5ea3fdbaa806acbc70f3709d866
Michail <Michail@DESKTOP-A488VHN>
2025-02-22 18:51:24 +0700

    Создание пустого решения

---
+++ LearnASPNET/LearnASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
