cf103e342199e28628450d9af4dc3e7f4d15cca8
Bertonipwnz <Bertonipwnz@github.com>
2025-02-22 20:22:38 +0700

    Подключил недостающие nuget пакеты в стартовом проекте

---
+++ LearnASPNETMVC/LearnASPNETMVC.WebApi/Controllers/AppController.cs
@@ -0,0 +1,20 @@
+﻿namespace LearnASPNETMVC.WebApi.Controllers
+{
+	using System.Web.Http;
+
+	/// <summary>
+	/// Контроллер приложения.
+	/// </summary>
+	public class AppController : ApiController
+	{
+		/// <summary>
+		/// Получает имя пользователя.
+		/// </summary>
+		/// <returns>Строковое представления пользователя.</returns>
+		[HttpGet]
+		public string GetUserName()
+		{
+			return "My user name";
+		}
+	}
+}
