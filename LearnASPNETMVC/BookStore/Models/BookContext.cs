9093203b0738f74b82e615a577bf5219ea10bf36
Bertonipwnz <Bertonipwnz@github.com>
2025-02-23 11:20:34 +0700

    Добавление контекста и строки подключения

---
+++ LearnASPNETMVC/BookStore/Models/BookContext.cs
@@ -0,0 +1,20 @@
+﻿namespace BookStore.Models
+{
+	using System.Data.Entity;
+	
+	/// <summary>
+	/// Контекст данных для книг.
+	/// </summary>
+	public class BookContext :DbContext
+	{
+		/// <summary>
+		/// Таблица книг.
+		/// </summary>
+		public DbSet<Book> Books { get; set; }
+
+		/// <summary>
+		/// Таблица покупок.
+		/// </summary>
+		public DbSet<Purchase> Purchases { get; set; }
+	}
+}
