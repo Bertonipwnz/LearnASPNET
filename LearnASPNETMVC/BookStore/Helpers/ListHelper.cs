5c563eaaddf56f9a20eedaa1f4c9b6778ef9e859
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-02-25 20:13:52 +0700

    Списочный хелпер

---
+++ LearnASPNETMVC/BookStore/Helpers/ListHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace BookStore.Helpers
+{
+	using System.Web.Mvc;
+
+	/// <summary>
+	/// Хелпер по работе со списками.
+	/// </summary>
+	public static class ListHelper
+	{
+		/// <summary>
+		/// Создает список.
+		/// </summary>
+		/// <param name="html">Html хелпер.</param>
+		/// <param name="items">Элементы.</param>
+		/// <returns>Строка Html.</returns>
+		public static MvcHtmlString CreateList(this HtmlHelper html, string[] items)
+		{
+			TagBuilder ul = new TagBuilder("ul");
+			foreach (string item in items)
+			{
+				TagBuilder li = new TagBuilder("li");
+				li.SetInnerText(item);
+				ul.InnerHtml += li.ToString();
+			}
+			return new MvcHtmlString(ul.ToString());
+		}
+	}
+}
