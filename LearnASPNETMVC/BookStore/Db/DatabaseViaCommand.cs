4edbeb3a69b19a0ce0fd5775f974a8176bd0ac9b
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-02-26 21:29:21 +0700

    Реализация создания таблицы через команды

---
+++ LearnASPNETMVC/BookStore/Db/DatabaseViaCommand.cs
@@ -0,0 +1,40 @@
+﻿namespace BookStore.Db.Contexts
+{
+	using System;
+	using System.Data.SqlClient;
+
+	/// <summary>
+	/// База данных.
+	/// </summary>
+	public class DatabaseCommand
+	{
+		private static string _connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\\Bookstore.mdf';Integrated Security=True";
+
+		private const string BOOK_TABLE_NAME = "book";
+
+		public static void CreateBookTable()
+		{
+			string sqlQuery = $@"CREATE TABLE {BOOK_TABLE_NAME} (
+                        Id INT PRIMARY KEY IDENTITY,
+                        Name NVARCHAR(100),
+                        Age INT
+                    )";
+
+			try
+			{
+				using (var connection = new SqlConnection(_connectionString))
+				{
+					connection.Open();
+					using (var command = new SqlCommand(sqlQuery, connection))
+					{
+						command.ExecuteNonQuery();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+	}
+}
--- LearnASPNETMVC/BookStore/Global.asax.cs
+++ LearnASPNETMVC/BookStore/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿namespace BookStore
 {
 	using BookStore.Db;
+	using BookStore.Db.Contexts;
 	using System.Data.Entity;
 	using System.Web.Mvc;
 	using System.Web.Optimization;
@@ -11,7 +12,7 @@
 		protected void Application_Start()
 		{
 			Database.SetInitializer(new BookDbInitializer());
-
+			DatabaseViaCommand.CreateBookTable();
 			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
